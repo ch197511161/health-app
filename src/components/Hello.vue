196e40f21d316f30c742e133b178541d7f820088
ch_01 <ch_01@21cn.com>
2017-06-20 01:34:47 +0800

    init2

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,13 +1,40 @@
 <template>
-  <image></image>
+  <div >
+    <div style="position: absolute;width:100%; height:100%; z-index: -1;background-color: #42b983">
+      <img src="/static/image/0_0启动页面.jpg" height="100%" width="100%" >
+    </div>
+     <div style="position: absolute;width:100%; height:100%">
+       <mt-button style="top: 60%" @click="handleClick">按钮</mt-button>
+      </div>
+    </div>
+  </div>
 </template>
-
 <script>
+import { Toast } from 'mint-ui'
+import axios from 'axios'
+// var aa = {}
+var instance = axios.create({
+  baseURL: 'http://127.0.0.1:10010/',
+  timeout: 1000,
+  headers: {'X-Custom-Header': 'foobar'}
+})
 export default {
-
   data () {
     return {
-
+    }
+  },
+  mounted: function () {
+    instance.get('/hello?name=Scott')
+    .then(function (response) {
+      console.log(response)
+    })
+  .catch(function (error) {
+    console.log(error)
+  })
+  },
+  methods: {
+    handleClick: function () {
+      Toast('提示信息')
     }
   }
 }
@@ -23,12 +50,10 @@ ul {
   list-style-type: none;
   padding: 0;
 }
-
 li {
   display: inline-block;
   margin: 0 10px;
 }
-
 a {
   color: #42b983;
 }
